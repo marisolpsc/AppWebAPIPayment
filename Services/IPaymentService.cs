921c490335e1893542c075607bc51bb417b056fa
marisolpsc <marisol_ps@live.com.mx>
2022-08-02 12:48:37 -0500

    Declaration of methods in service

---
+++ Services/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using WebAppPayments.Models;
+using WebAppPayments.Models.Request;
+
+namespace WebAppPayments.Services
+{
+    public interface IPaymentService
+    {
+        IQueryable<Payment> GetPayments();
+        bool CreatePayments(PaymentRequest model);
+        bool UpdatePayments(PaymentRequest model);
+        bool DeletePayments(int PaymentId);
+        
+
+    }
+}
