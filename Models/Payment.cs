e2233e7e489dacd7aa467c4b51ce7f72b670f76d
marisolpsc <marisol_ps@live.com.mx>
2022-08-02 12:48:52 -0500

    Declaration of methods in service

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace WebAppPayments.Models
 {
     public partial class Payment
     {
         public int PaymentId { get; set; }
-        public int ClientId { get; set; }
+        public virtual Client Client { get; set; } = null!;
+        public virtual PaymentType PaymentType { get; set; } = null!;
         public DateTime? PaymentDate { get; set; }
         public string? PaymentDescription { get; set; }
         public int? PaymentAmount { get; set; }
+        
+        [IgnoreDataMember]
+        [JsonIgnore]
+        public int ClientId { get; set; }
+        [IgnoreDataMember]
+        [JsonIgnore]
         public int PaymentTypeId { get; set; }
 
-        public virtual Client Client { get; set; } = null!;
-        public virtual PaymentType PaymentType { get; set; } = null!;
+        
+       
     }
 }
